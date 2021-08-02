90f1be5f6ced373d4597d19d0515e30508b91c88
Alexander Janssen <a.janssen@easy-cloud.eu>
2021-08-02 15:16:09 +0200

    Support PHP 8 attributes (#20)

--- Validator/Constraints/Vatin.php
+++ Validator/Constraints/Vatin.php
@@ -9,10 +9,37 @@ use Symfony\Component\Validator\Constraint;
  *
  * @Annotation
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
 class Vatin extends Constraint
 {
     public $message = 'This is not a valid VAT identification number';
     public $checkExistence = false;
+
+    /**
+     * Vatin constructor.
+     *
+     * @param array|null  $options
+     * @param string|null $message
+     * @param bool|null   $checkExistence
+     * @param array|null  $groups
+     * @param null        $payload
+     */
+    public function __construct(
+        array $options = null,
+        string $message = null,
+        bool $checkExistence = null,
+        array $groups = null,
+        $payload = null
+    ) {
+        if ($message) {
+            $options['message'] = $message;
+        }
+        if ($checkExistence) {
+            $options['checkExistence'] = $checkExistence;
+        }
+
+        parent::__construct($options ?? [], $groups, $payload);
+    }
 
     public function validatedBy()
     {
