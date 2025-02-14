ba9d1fd7381c1e79ff7ae59a821b918b7d6214cb
Jakub Caban <kuba.iluvatar@gmail.com>
2025-02-14 11:30:27 +0100

    Switch to correct Symfony Extension namespace (#28)

--- src/DependencyInjection/DdeboerVatinExtension.php
+++ src/DependencyInjection/DdeboerVatinExtension.php
@@ -4,7 +4,7 @@ namespace Ddeboer\VatinBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
